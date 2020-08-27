370321bd12f0afd00c1d9b5351e130318bfcfb3d
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-27 10:00:13 -0300

    fix input component

--- src/components/AuthInput/styles.ts
+++ src/components/AuthInput/styles.ts
@@ -8,25 +8,30 @@ export const Container = styled.div`
   justify-content: flex-start;
   border: 1px solid #e6e6f0;
   border-radius: 0.4rem;
-  padding: 1.5rem;
+  padding: 1rem;
   position: relative;
   margin-bottom: 1rem;
   transition: border-left 0.4s ease-in-out;
+  border-left: 1px solid #e6e6f0;
 
   div {
     display: flex;
     flex-direction: column;
+    width: 100%;
   }
-
-  border-left: 1px solid #e6e6f0;
 
   input {
     border: 0;
     font-size: 1.4rem;
+    padding-top: 0.5rem;
+    display: flex;
+    justify-self: stretch;
   }
 
   label {
     position: absolute;
+    display: flex;
+    justify-self: center;
     font-size: 1.4rem;
     color: var(--color-text-complement);
     transition: all 0.4s ease;
@@ -37,8 +42,7 @@ export const Container = styled.div`
   }
 
   &:focus-within label {
-    top: -1px;
-    left: 1rem;
+    transform: translateY(-50%);
     font-size: 1rem;
   }
 

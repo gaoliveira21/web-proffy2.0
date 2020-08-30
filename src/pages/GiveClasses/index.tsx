31d8c86d28696cc9bd76d967de9a127e19668435
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-30 17:37:13 -0300

    give classes form

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -4,7 +4,7 @@ import HeaderNavigation from '../../components/HeaderNavigation/index';
 
 import rocket from '../../assets/images/icons/rocket.svg';
 
-import { Container, Header } from './styles';
+import { Container, Header, Main, AvatarBlock } from './styles';
 
 const GiveClasses: React.FC = () => {
   return (
@@ -21,6 +21,41 @@ const GiveClasses: React.FC = () => {
           </p>
         </div>
       </Header>
+      <Main>
+        <form>
+          <fieldset>
+            <legend>Seus dados</legend>
+            <div className="input-block">
+              <AvatarBlock>
+                <img
+                  src="https://api.adorable.io/avatars/285/abott@adorable.png"
+                  alt="Avatar"
+                />
+                <p>Gabriel José</p>
+              </AvatarBlock>
+              <div>
+                <label htmlFor="whatsapp">Whatsapp</label>
+                <input type="text" id="whatsapp" />
+              </div>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="bio">
+                Biografia <small>(Máximo 300 caracteres)</small>
+              </label>
+              <textarea name="bio" id="bio" rows={10} maxLength={300} />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Sobre a aula</legend>
+          </fieldset>
+
+          <fieldset>
+            <legend>Horários disponíveis</legend>
+          </fieldset>
+        </form>
+      </Main>
     </Container>
   );
 };
--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div`
 export const Header = styled.header`
   background: var(--color-primary);
   width: 100%;
-  padding: 4rem 10%;
+  padding: 4rem 6% 8rem;
 
   h2 {
     font-family: Archivo;
@@ -42,7 +42,7 @@ export const Header = styled.header`
   }
 
   @media (min-width: 1100px) {
-    padding: 4rem 20%;
+    padding: 4rem 20% 8rem;
 
     > div {
       flex-direction: row;
@@ -52,5 +52,88 @@ export const Header = styled.header`
         justify-content: flex-end;
       }
     }
+  }
+`;
+
+export const Main = styled.main`
+  padding: 0 6% 8rem;
+  width: 100%;
+  height: 500px;
+
+  form {
+    width: 100%;
+    background: #fff;
+
+    margin-top: -4rem;
+    border-radius: 0.8rem;
+    padding: 4rem;
+
+    fieldset {
+      border: 0;
+      margin-bottom: 4rem;
+
+      legend {
+        width: 100%;
+        font-family: Archivo;
+        font-weight: bold;
+        font-size: 2.4rem;
+        color: var(--color-text-title);
+        padding-bottom: 1.6rem;
+        border-bottom: 1px solid var(--color-line-in-white);
+      }
+
+      > div.input-block {
+        display: flex;
+        flex-direction: column;
+
+        > div {
+          display: flex;
+          flex-direction: column;
+        }
+
+        label {
+          font-family: Poppins;
+          color: var(--color-text-complement);
+          font-size: 1.6rem;
+          padding: 1rem 0;
+        }
+
+        input,
+        textarea {
+          width: 100%;
+          padding: 1.2rem;
+          background: var(--color-input-background);
+          border: 1px solid var(--color-line-in-white);
+          border-radius: 0.8rem;
+          margin-bottom: 1rem;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 1100px) {
+    padding: 0 20% 8rem;
+  }
+`;
+
+export const AvatarBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  position: relative;
+
+  img {
+    width: 50%;
+    height: 50%;
+    border-radius: 50%;
+  }
+
+  p {
+    font-size: 2.2rem;
+    color: var(--color-text-title);
+    font-family: Archivo;
+    font-weight: bold;
+    padding: 1.5rem 0;
   }
 `;
